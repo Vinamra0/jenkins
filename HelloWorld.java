8a94ce47eebea288116995c2b3f7749ee895d2ca
Vinamra <singh001vinamra@gmail.com>
2023-08-14 20:11:33 +0530

    Changed some things

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 class HelloWorld{
     
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World \n");
+        System.out.println("You rock my world!! \n");
+        System.out.println("My world is rocked, now please stop \n");
     }
 }
